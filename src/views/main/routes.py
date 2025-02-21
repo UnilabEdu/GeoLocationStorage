768019847204d34d70541b9ca507e5d135e186c4
NikaL25 <mr.lomiashvil@mail.ru>
2025-02-21 19:46:29 +0400

    Added Pagination and Data in DB

--- src/views/main/routes.py
+++ src/views/main/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request
 from os import path
 from src.config import Config
+from src.models.location import Location
 
 TEMPLATES_FOLDER = path.join(Config.BASE_DIRECTORY, "templates", "main")
 
@@ -8,7 +9,11 @@ main_blueprint = Blueprint("main", __name__, template_folder=TEMPLATES_FOLDER)
 
 @main_blueprint.route('/')
 def index():
-    return render_template('index.html')
+    page = request.args.get('page', 1, type=int)
+    per_page = 4
+    pagination = Location.query.order_by(Location.id.desc()).paginate(page=page, per_page=per_page, error_out=False)
+
+    return render_template('index.html', locations=pagination.items, pagination=pagination)
 
 @main_blueprint.route('/about-project')
 def about():
--- src/views/pages/routes.py
+++ src/views/pages/routes.py
@@ -1,15 +1,33 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request
 from os import path
 from src.config import Config
+from src.models.bibliography import Bibliography
+from src.models import Location
+
+
 TEMPLATES_FOLDER = path.join(Config.BASE_DIRECTORY, "templates", "pages")
 
 pages_bp = Blueprint("pages", __name__, template_folder=TEMPLATES_FOLDER)
 
-@pages_bp.route('/list_of_places')
-def locations():
-    return render_template('list_of_places.html')
-
-
 @pages_bp.route('/bibliography')
 def bibliography():
-    return render_template('bibliography.html')
+    page = request.args.get('page', 1, type=int)
+    per_page = 10
+
+    bibliographies = Bibliography.query.order_by(Bibliography.id.desc()).paginate(page=page, per_page=per_page, error_out=False)
+
+    return render_template('bibliography.html', bibliographies=bibliographies)
+
+
+@pages_bp.route('/list_of_places')
+def locations():
+    page = request.args.get('page', 1, type=int)
+    per_page = 20
+    pagination = Location.query.paginate(page=page, per_page=per_page, error_out=False)
+    return render_template('list_of_places.html', locations=pagination.items, pagination=pagination)
+
+
+@pages_bp.route('/place/<int:location_id>')
+def place_detail(location_id):
+    location = Location.query.get_or_404(location_id)
+    return render_template('places-inside-page.html', location=location)
